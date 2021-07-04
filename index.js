2cc4c049d17fcd042caabb8b2128f5357fca8a52
kritik-Verma <85352603+kritik-Verma@users.noreply.github.com>
2021-07-05 02:23:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://www.youtube.com/results?search_query=no+loss+king");
-  const result = await page.evaluate(() => {
-    const htmlElement = document.getElementsByClassName(
-      "yt-simple-endpoint style-scope ytd-video-renderer"
-    )[0];
-    const arrOfElements = [...htmlElement];
-    console.log("songurl");
-    // const songurl = arrOfElements[0].href;
-    return arrOfElements.href;
-  });
+const funk = async (qureySting) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(
+        `https://www.youtube.com/results?search_query=${qureySting}`
+    );
+    const result = await page.evaluate(() => {
+        const htmlElement = document.getElementsByClassName(
+            'yt-simple-endpoint style-scope ytd-video-renderer'
+        )[0];
+        const arrOfElements = htmlElement;
+        console.log('songurl');
+        return arrOfElements.href;
+    });
+    const temp = result;
+    // console.log(temp);
+    await browser.close();
+    return temp;
+};
 
-  console.log(result);
-
-  await browser.close();
-})();
+module.exports = funk;
